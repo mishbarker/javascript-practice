cf2c030fe83a2b22f86b0a89879cc84763af507d
Michelle Barker <barkerwebdev47@gmail.com>
2021-07-01 20:01:26 -0700

    test commit

--- day-01/day-02/js/overview.js
+++ day-01/day-02/js/overview.js
@@ -11,7 +11,7 @@ const objects = {
     boolean: boolean,
     body: {
         style: {
-            backgroundColor: 'green',
+            backgroundColor: 'blue',
             color: 'black'
         }
     },
