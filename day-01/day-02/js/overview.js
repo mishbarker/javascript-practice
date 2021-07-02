5de215f47bcd372489a3947137cc5590c3488c9e
Michelle Barker <barkerwebdev47@gmail.com>
2021-07-01 20:22:36 -0700

    pink

--- day-01/day-02/js/overview.js
+++ day-01/day-02/js/overview.js
@@ -11,7 +11,7 @@ const objects = {
     boolean: boolean,
     body: {
         style: {
-            backgroundColor: 'green',
+            backgroundColor: 'pink',
             color: 'black'
         }
     },
